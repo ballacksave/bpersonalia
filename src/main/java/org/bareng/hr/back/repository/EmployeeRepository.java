cd62af3eabc732ad8b6a1d3a5833244a65c60008
ariefhasan <ariefhasan@users.noreply.github.com>
2015-03-12 09:04:00 +0700

    Update EmployeeRepository.java

--- src/main/java/org/bareng/hr/back/repository/EmployeeRepository.java
+++ src/main/java/org/bareng/hr/back/repository/EmployeeRepository.java
@@ -5,8 +5,14 @@ import org.apache.ibatis.annotations.Select;
 import org.bareng.hr.back.entity.Employee;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository {
+	
+	@Select("select * from t_employee")
+	List<Employee> findAll();
+	
 	@Select("select * from t_employee emp where emp.id=#employeeId")
 	Employee findOneById(@Param("employeeId") Integer employeeId);
 }
